5b005c6a13ff055f3772b35cfe7b40a098e087af
Thomas <twisse@hotmail.com>
2021-05-27 15:33:09 +0200

    clean up of code, added slightly changed data of testrun based on newer code

--- src/commonFirstLayerGoForInteractors.py
+++ src/commonFirstLayerGoForInteractors.py
@@ -2,24 +2,20 @@
 """
 Created on Fri Dec 11 11:57:47 2020
 
-Working version to find all 1st layer children GO terms for interacting genes
-
-How do we want to save this? check if data types are logical and useful.
-Manually check if the output is correct...
+Working version to find all 1st layer children GO terms for interacting genes and compare this with common interactor count 
 
 @author: Thomas
 """
 
-from timeit import default_timer as timer
 
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+
 import numpy as np
 from collections import defaultdict 
 import pandas as pd
 import os
 import seaborn as sns
 from datetime import date
+# import matplotlib.pyplot as plt
 
 fileDir = os.path.dirname(os.path.abspath(__file__)) # get path to this file
 os.chdir(fileDir) # Change working directory to current file
@@ -31,13 +27,12 @@ from modulesT import common_interactors_T
 from modulesT import childrenFromGoTerms
 from modulesT import goTermsFromGenes
 
-# from python_modules.module_common_measures import common_partners
 #%% User settings
 save = False #Set if data should be saved to excel
 if save == True:
     dateToday = str(date.today())
 
-#%% getting the data
+#%% getting locally saved data
 
 # data_go = pd.read_excel('../data/slim-goterms-filtered-data.xlsx') #Switched to get data from Yeastmine __> currently unused # Get pre-downloaded go slim data for many genes
 # data_go = data_go.dropna()
@@ -46,46 +41,37 @@ if save == True:
 # data_go = data_go.drop(['gene-id', 'go-aspect','go-id','feature-type'], axis = 1) #drop unused columns
 
 data=pd.read_excel('../data/data-BioGrid-Yeast.xlsx') #Get pre-downloaded genetic interaction data
-data = data.drop(['paper-source'], axis = 1) #drop unused column. SHOULD ALSO DROP INTERACTION THAT ARE NOT SL, NG or PG! Currently this is done before plotting.
-#%% query
+data = data.drop(['paper-source'], axis = 1) #drop unused column. NOTE: Currently unused interactions are dropped before plotting. Doing it here instead may shorten run times
+#%% Set query gene(s)
 
-query = ['BEM1', 'BEM3'] #testing query
+# query = ['BEM1', 'BEM3'] #testing query
 
-# query = pd.read_excel('../data/genesCellPolarity_SGD_amigo.xlsx') #gene set to investigate. Example: cell polarity genes
-# query.columns=['gene']
-# query = list(set(query.gene)) #get unique entries (remove duplicates in query list)
-
-
-#%% (TESTING) Reduce dataset to only query genes
-
-# data_go_temp = data_go[data_go['Gene'].isin(query)] #take only GO data of genes which are in the query
-# data_temp = data[data['gene-query-name'].isin(query)] #take only interaction data of genes which are in the query. Currently unused
-
+query = pd.read_excel('../data/genesCellPolarity_SGD_amigo.xlsx') #gene set to investigate. Example: cell polarity genes
+query.columns=['gene']
+query = list(set(query.gene)) #get unique entries (remove duplicates in query list)
 
 #%% Calling the function common_interactors
-# start = timer()
-commonInteractorData=common_interactors_T(query,data) #find all common interactors of each of the query genes (with all yeast genes). Based on supplied interaction data
-# end = timer()
-# print('Duration of determining interactors of query genes function = ' + str(round(end-start)) + 's') #print duration of function common_interactors_T
-uniqueInteractorGenes = list(set(commonInteractorData['interactorGene'])) #Get unique interactors (remove duplicates to reduce the list of interactor genes to unique genes)
 
+# Possible investigation idea: reduce the interaction gene set to just those contained in the query
+
+commonInteractorData=common_interactors_T(query,data) #find all common interactors of each of the query genes (with all yeast genes). Based on supplied interaction data
+uniqueInteractorGenes = list(set(commonInteractorData['interactorGene'])) #Get unique interactors (remove duplicates to reduce the list of interactor genes to unique genes)
 
 
 #%% get go term data for genes (Set up for testing: only take the first x genes)
 
-goTermsInteractors = goTermsFromGenes(uniqueInteractorGenes[0:4])
+goTermsInteractors = goTermsFromGenes(uniqueInteractorGenes)
 goTermsQuery = goTermsFromGenes(query)
-# goTermsInteractors.columns=['go-term']
-# goTermsQuery.columns=['go-term']
 
 
 #%% getting 1st layer children of GO terms to file (Set up for testing: only take the first x genes)
 
-# save all 1st layer children for a gene or save all 1st layer children for a GO terms? latter would take more time later but saves runtime here...
-# may also just do this for all 6000 genes so we are done...
-# this fucntion takes essentially forever --> find a better way. maybe download all children for GO terms so we don't have to use yeastmine everytime?
+# this fucntion takes essentially forever. Maybe download all children for GO terms so we don't have to use yeastmine everytime?
+# save all 1st layer children for a gene or save all 1st layer children for all GO terms? latter would probably take too much time later but once done would save a lot of runtime...
+# may also just do this for all 6000 genes so we are done
 
-fLCInteractors = childrenFromGoTerms(uniqueInteractorGenes[0:4],goTermsInteractors) #fLC = first layer children. 
+
+fLCInteractors = childrenFromGoTerms(uniqueInteractorGenes,goTermsInteractors) #fLC = first layer children. 
 fLCQuery = childrenFromGoTerms(query,goTermsQuery)
 
 #%% return unique interactor list to per interacting genes
@@ -93,11 +79,11 @@ fLCQuery = childrenFromGoTerms(query,goTermsQuery)
 commonInteractorSetData = defaultdict(dict)
 
 for ii in commonInteractorData['commonInteractors'].to_dict():
-    interactorGene = ii.split("-",1)[1]
-    commonInteractorSetData[ii] = fLCInteractors[interactorGene]
+    commonInteractorSetData[ii] = fLCInteractors[ii.split("-",1)[1]]
 
 
-#%% saving 1st layer children data to excel (using dataframes, if this turns out to be an issue switch to csv package maybe)
+#%% saving 1st layer children data to excel (using dataframes, if this turns out to be an issue maybe we can switch to csv package)
+#np.savetxt(r'c:\data\np.txt', df.values, fmt='%d') ALTERNATIVE: SAVE TO TXT
 
 if save == True:
     df=pd.DataFrame([fLCInteractors]).T
@@ -105,9 +91,6 @@ if save == True:
     
     df.to_excel(r'../data/' + dateToday + '_1stLayerGO_INT_BEM1_BEM3_testing.xlsx', index = True)
     df2.to_excel(r'../data/' + dateToday + '_1stLayerGO_BEM1_BEM3_testing.xlsx', index = True)
-
-#np.savetxt(r'c:\data\np.txt', df.values, fmt='%d') ALTERNATIVE: SAVE TO TXT. MAY BE WORTH LOOKING INTO
-
 
 #%% load data saved above
 
@@ -118,12 +101,10 @@ if save == True:
 # data_childrenGoTermsQuery.columns = ['Gene','ChildGoTerms']
 
 
-#%% Find the overlap of all 1st layer children of the GO Terms corresponding to the query gene(s) and its interactors
+#%% Find the overlap of all 1st layer children of the GO Terms corresponding to the query gene(s) and its/their interactors
 
 commonChildGoTerms = defaultdict(dict)
 commonChildGoTermsFraction = defaultdict(dict)
-
-genes = list(fLCInteractors.keys())
 
 jj = 0
 
@@ -154,48 +135,42 @@ if save == True:
 
 
 #%% consolidate data for plotting
-
 # interactionType = dict(zip(data['gene-query-name']+'-'+ data['gene-target-name'], data['interaction-type'])) #transform dataframe to dict with keys synthesised from two colums
 
 dataCommonGo = commonInteractorData
 dataCommonGo['commonChildGoTermsFraction']= dataCommonGo.index.to_series().map(commonChildGoTermsFraction) #map by index to ensure correct order
-dataCommonGo['commonInteractorCount'] = list(map(len,list(commonInteractorData['commonInteractors']))) #superfluous or not?
-#dataCommonGo['interactionType'] = commonInteractorData['interactionType']
+dataCommonGo['commonInteractorCount'] = list(map(len,list(commonInteractorData['commonInteractors'])))
 
 #%% select which data to plot
-gene = 'BEM1'
+gene = 'BEM1' #Plot data of this gene
+
 plotData = dataCommonGo[dataCommonGo['queryGene']==gene]
 
-
-plotDataReduced = plotData[np.array(plotData['interactionType'] == 'Negative Genetic') | \
+plotData = plotData[np.array(plotData['interactionType'] == 'Negative Genetic') | \
                            np.array(plotData['interactionType'] == 'Positive Genetic') | \
                            np.array(plotData['interactionType'] == 'Synthetic Lethality')] #only select specific interaction types
 
-# keys = list(commonChildGoTerms.keys())
-# queryKeys = [i for i in keys if gene in i] #Get a list all keys containing the string <gene> 
-# fractionCommonGO = list(map(commonChildGoTermsFraction.get, queryKeys)) #get all fractions for query keys
 
 
-#%% Plotting
+#%% Plotting (1/2):
+# sns.set(style="ticks", color_codes=True)
+# plot=sns.pairplot(plotData,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType', \
+#                     hue_order=['Negative Genetic','Positive Genetic','Synthetic Lethality'], \
+#                     diag_kind="hist", diag_kws = {'bins':int(np.ceil(np.sqrt(plotData.shape[0])))},corner=True)
+# plot=sns.pairplot(plotData,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType',
+#                    corner=True, diag_kws = {'bw' : 10, 'kernel' : 'tri'})
+# plot.fig.suptitle(gene)
+
+
+#%% Plotting (2/2): (Alternative)
+
+# bins = int(np.ceil(np.sqrt(plotData.shape[0])))
 sns.set(style="ticks", color_codes=True)
-plot=sns.pairplot(plotDataReduced,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType', \
-                   hue_order=['Negative Genetic','Positive Genetic','Synthetic Lethality'], \
-                   diag_kind="hist", diag_kws = {'bins':int(np.ceil(np.sqrt(plotDataReduced.shape[0])))},corner=True)
-# plot=sns.pairplot(plotDataReduced,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType',
-                  # corner=True, diag_kws = {'bw' : 10, 'kernel' : 'tri'})
-plot.fig.suptitle(gene)
-
-
-#%% Plotting results: Combined plot
-
-# gene = 'BEM1'
-# bins = int(np.ceil(np.sqrt(tmp.shape[0])))
-sns.set(style="ticks", color_codes=True)
-plot=sns.pairplot(plotDataReduced,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType', \
+plot=sns.pairplot(plotData,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType', \
                     hue_order=['Negative Genetic','Positive Genetic','Synthetic Lethality'], \
-                    diag_kind="hist",diag_kws = {'bins':int(np.ceil(np.sqrt(plotDataReduced.shape[0])))},corner=True)
-# plot=sns.pairplot(plotDataReduced,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType', \
-#                    hue_order=['Negative Genetic','Positive Genetic','Synthetic Lethality'])
+                    diag_kind="hist",diag_kws = {'bins':int(np.ceil(np.sqrt(plotData.shape[0])))},corner=True)
+# plot=sns.pairplot(plotData,vars=['commonChildGoTermsFraction','commonInteractorCount'],hue='interactionType', \
+#                     hue_order=['Negative Genetic','Positive Genetic','Synthetic Lethality'])
 # plt.title(query[0])
-plot.fig.suptitle('BEM1',y=1.08)
+plot.fig.suptitle(gene,y=1.08)
 # # plot.savefig('../output_images/Testing/common-go-terms-of-'+ gene +'-based-on-their-type.png',dpi=300,format='png',transparent=True)
